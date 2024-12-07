7954af7b9884289cf3b5691be0c33ad839fc2f41
DewzaCSharp <ayv0cracked@outlook.de>
2024-12-07 17:05:16 +0100

    Delete mmh.bat

--- mmh.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-git init
-git add README.md
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/DewzaCSharp/Big-File-Creator.git
-git push -u origin main
-pause
